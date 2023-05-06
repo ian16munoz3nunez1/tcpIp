bbd90a1af2bca5c02f08187cf220fc7e336e8210
Ian Muñoz Nuñez <ianemn@ian.kali>
2023-05-06 01:37:50 -0600

    2023-05-06_01:38 --> Actualizacion de manual y arranque
    Se actualizó la información del manual, como actualización o
    cambio de banderas, nuevas banderas y comandos nuevos. También
    se actualizó el comando para correr el script, ahora es más
    sencillo correr pruebas en el servidor local.

--- client/main.py
+++ client/main.py
@@ -3,26 +3,32 @@ import sys
 from tcp import TCP
 from man import logo, man
 
+if len(sys.argv) == 1:
+    host = "127.0.0.1"
+    port = 9999
+
+    tcp = TCP(host, port)
+    tcp.conectar()
+    tcp.shell()
+
 if len(sys.argv) == 2:
-    if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+    if sys.argv[1] == "--help" or sys.argv[1] == "-h":
         logo()
         man()
     else:
-        print("Error de sintaxis")
+        exit()
 
 elif len(sys.argv) == 3:
     try:
         host = sys.argv[1]
         port = int(sys.argv[2])
+
         tcp = TCP(host, port)
         tcp.conectar()
         tcp.shell()
     except:
-        print("Error de sintaxis")
+        exit()
 
 else:
-    tcp = TCP("localhost", 9999)
+    exit()
 
-    tcp.conectar()
-
-    tcp.shell()
--- server/main.py
+++ server/main.py
@@ -2,13 +2,24 @@
 from tcp import TCP
 from man import logo, man
 import sys
+from colorama import init
+from colorama.ansi import Fore
 
-if len(sys.argv) == 2:
+init(autoreset=True)
+
+if len(sys.argv) == 1:
+    host = "0.0.0.0"
+    port = 9999
+
+    tcp = TCP(host, port)
+    tcp.shell()
+
+elif len(sys.argv) == 2:
     if sys.argv[1] == "--help" or sys.argv[1] == "-h":
         logo()
         man()
     else:
-        print("Error de sintaxis")
+        print(Fore.RED + "[-] Error de sintaxis")
 
 elif len(sys.argv) == 3:
     try:
@@ -18,12 +29,8 @@ elif len(sys.argv) == 3:
         tcp = TCP(host, port)
         tcp.shell()
     except:
-        print("Error de sintaxis")
+        print(Fore.RED + "[-] Error de sintaxis")
 
 else:
-    host = sys.argv[1]
-    port = int(sys.argv[2])
+    print(Fore.YELLOW + f"[!] Demasiados argumentos ({len(sys.argv)})")
 
-    tcp = TCP("0.0.0.0", 9999)
-
-    tcp.shell()
